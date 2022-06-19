6d972face31c93811f88c3887cd9e89e0b47b13e
ohyeryung <100130070+ohyeryung@users.noreply.github.com>
2022-06-19 21:30:05 +0900

    first commit

---
+++ src/main/java/com/example/algorithm/Hash1.java
@@ -0,0 +1,43 @@
+package com.example.algorithm;
+
+import java.util.HashMap;
+
+public class Hash1 {
+    public static void main(String[] args) {
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"kiki", "eden"};
+        Hash1 solution = new Hash1();
+        System.out.println(solution.Solution(participant,completion));
+    }
+    public String Solution(String[] participant, String[] completion) {
+        HashMap<String ,Integer> h = new HashMap<String ,Integer>();
+        int tmp=0;
+        for(String s: completion) {
+            // System.out.println(h.get(s));
+            if(h.get(s) != null) {
+                tmp = h.get(s);
+                h.put(s,tmp+1);
+            }
+            else {
+                h.put(s,1);
+            }
+        }
+        // System.out.println(h.toString());
+
+        for(String s:participant) {
+            if(h.get(s) != null) {
+                tmp = h.get(s);
+                if(tmp>0)
+                    h.put(s,tmp-1);
+                else {
+                    return s;
+                }
+            }
+            else {
+                return s;
+            }
+        }
+        return "Error";
+
+    }
+}
---
+++ src/main/java/com/example/algorithm/Hash2.java
@@ -0,0 +1,45 @@
+package com.example.algorithm;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hash2 {
+    public static void main(String[] args) {
+        String[] phone_book = {"119", "97674223", "1195524421"};
+        Hash2 solution = new Hash2();
+        System.out.println(solution.Solution(phone_book));
+    }
+
+    // Hash 안쓰고 sort해서 푸는 방법
+//    public boolean Solution(String[] phone_book) {
+//        Arrays.sort(phone_book);
+//
+//        // startsWith 함수 : 접두어 판단하는 함수!
+//        for (int i = 0; i < phone_book.length - 1; i++)
+//            if (phone_book[i + 1].startsWith(phone_book[i]))
+//                return false;
+//        return true;
+//    }
+
+    // Hash
+    public boolean Solution(String[] phone_book) {
+
+        // 1. 빈 HashMap 선언
+        Map<String, Integer> hs = new HashMap<>();
+
+        // 2. 모든 전화번호를 HashMap에 넣기 (Key값만으로 비교가능하므로 value값은 관계 X!)
+        for (int i = 0; i < phone_book.length; i++) {
+            hs.put(phone_book[i],i);
+        }
+
+        for (int i = 0; i < phone_book.length; i++) {
+
+            for (int j = 0; j < phone_book[i].length(); j++) {
+                if (hs.containsKey(phone_book[i].substring(0,j)))
+                    return false;
+            }
+        }
+        return true;
+    }
+}
